112fa55da1f1beb6cbfd37186a0ec6f3f8035451
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-04-11 17:05:20 +0200

    Add different favicon to web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- favicon -->
+        <link rel="icon" href="{{ asset('favicon.png') }}">
+
         <title>Laravel</title>
 
         <!-- Fonts -->
